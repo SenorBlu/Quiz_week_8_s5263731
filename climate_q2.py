a777365c3a4d263364af4b5b6d286fcf20256993
SenorBlu <fa313rana@gmail.com>
2023-09-14 19:47:53 +1000

    loaded csv with pandas and displayed data, added tight layout because of layout issues.

--- climate_q2.py
+++ climate_q2.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-years = []
-co2 = []
-temp = []
+cd = pd.read_csv("climate.csv")
+years = cd["Year"]
+co2 = cd["CO2"]
+temp = cd["Temperature"]
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
@@ -15,6 +16,7 @@ plt.subplot(2, 1, 2)
 plt.plot(years, temp, 'r*-') 
 plt.ylabel("Temp (C)") 
 plt.xlabel("Year (decade)") 
+plt.tight_layout()
 plt.show() 
 plt.savefig("co2_temp_2.png") 
 
