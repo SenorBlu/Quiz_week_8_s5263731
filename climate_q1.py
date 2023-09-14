7c17df9ab56a78d6cbb89410d9e4b57405947c2d
SenorBlu <fa313rana@gmail.com>
2023-09-14 19:30:27 +1000

    Added sqlite3 and used it to load the climate.db and show the data

--- climate_q1.py
+++ climate_q1.py
@@ -1,8 +1,21 @@
 import matplotlib.pyplot as plt
-        
+import sqlite3 as sqla
+
 years = []
 co2 = []
 temp = []
+
+connection = sqla.connect("climate.db")
+cursor = connection.cursor()
+cursor.execute("SELECT Year, CO2, Temperature FROM ClimateData")
+data = cursor.fetchall()
+
+for table in data:
+    years.append(table[0])
+    co2.append(table[1])
+    temp.append(table[2])
+
+connection.close()
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
@@ -14,5 +27,8 @@ plt.subplot(2, 1, 2)
 plt.plot(years, temp, 'r*-') 
 plt.ylabel("Temp (C)") 
 plt.xlabel("Year (decade)") 
+plt.tight_layout()  
 plt.show() 
-plt.savefig("co2_temp_1.png") 
+plt.savefig("co2_temp_1.png")
+
+
